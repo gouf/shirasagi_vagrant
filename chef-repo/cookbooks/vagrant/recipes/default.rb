476684b495629283952475c33b2493d7c62cb323
gouf <innocent.zero@gmail.com>
2014-09-07 09:09:37 +0900

    update vagrant recipe, configure and add epel repository

--- chef-repo/cookbooks/vagrant/recipes/default.rb
+++ chef-repo/cookbooks/vagrant/recipes/default.rb
@@ -16,3 +16,12 @@ execute 'devtools' do
   command 'yum -y groupinstall "Development Tools"'
   action :run
 end
+
+yum_repository 'epel' do
+  baseurl 'http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/6/x86_64/'
+  mirrorlist 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=x86_64'
+  description 'Extra Packages for Enterprise Linux'
+  enabled true
+  gpgcheck true
+  gpgkey 'http://ftp.riken.jp/Linux/fedora/epel/RPM-GPG-KEY-EPEL-6'
+end
